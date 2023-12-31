adfe96a37a2f7c2cd63c0ff5b1aa03e70c238317
avronilrajib <avronilrajib@gmail.com>
2023-12-31 15:44:51 +0600

    adding footer section and responsive some section

---
+++ index.js
@@ -0,0 +1,9 @@
+const showSideBar=()=>{
+    const sideShow=document.querySelector(".sidebar");
+    sideShow.style.display="flex";
+
+}
+const closeSideBar=()=>{
+    const sideShow=document.querySelector(".sidebar");
+    sideShow.style.display="none";
+}
